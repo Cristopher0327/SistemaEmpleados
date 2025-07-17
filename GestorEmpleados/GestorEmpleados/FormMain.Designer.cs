3d42d12afd786431e20c8de400b30e5047256541
Cristopher0327 <sjoselito099@gmail.com>
2025-07-16 20:11:24 -0400

    Proyecto 4
    Actividad 4

---
+++ GestorEmpleados/GestorEmpleados/FormMain.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace GestorEmpleados
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnMostrarEmpleados = new System.Windows.Forms.Button();
+            this.btnAgregarEmpleado = new System.Windows.Forms.Button();
+            this.btnActualizarEmpleado = new System.Windows.Forms.Button();
+            this.btnEliminarEmpleado = new System.Windows.Forms.Button();
+            this.btnExportarCSV = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.Controls.Add(this.btnExportarCSV);
+            this.panelMenu.Controls.Add(this.btnSalir);
+            this.panelMenu.Controls.Add(this.btnActualizarEmpleado);
+            this.panelMenu.Controls.Add(this.btnEliminarEmpleado);
+            this.panelMenu.Controls.Add(this.btnAgregarEmpleado);
+            this.panelMenu.Controls.Add(this.btnMostrarEmpleados);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(238, 450);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // btnMostrarEmpleados
+            // 
+            this.btnMostrarEmpleados.Location = new System.Drawing.Point(23, 12);
+            this.btnMostrarEmpleados.Name = "btnMostrarEmpleados";
+            this.btnMostrarEmpleados.Size = new System.Drawing.Size(163, 51);
+            this.btnMostrarEmpleados.TabIndex = 0;
+            this.btnMostrarEmpleados.Text = "Mostrar empleados";
+            this.btnMostrarEmpleados.UseVisualStyleBackColor = true;
+            this.btnMostrarEmpleados.Click += new System.EventHandler(this.btnMostrarEmpleados_Click);
+            // 
+            // btnAgregarEmpleado
+            // 
+            this.btnAgregarEmpleado.Location = new System.Drawing.Point(23, 83);
+            this.btnAgregarEmpleado.Name = "btnAgregarEmpleado";
+            this.btnAgregarEmpleado.Size = new System.Drawing.Size(163, 51);
+            this.btnAgregarEmpleado.TabIndex = 1;
+            this.btnAgregarEmpleado.Text = "Agregar empleado";
+            this.btnAgregarEmpleado.UseVisualStyleBackColor = true;
+            this.btnAgregarEmpleado.Click += new System.EventHandler(this.btnAgregarEmpleado_Click);
+            // 
+            // btnActualizarEmpleado
+            // 
+            this.btnActualizarEmpleado.Location = new System.Drawing.Point(23, 155);
+            this.btnActualizarEmpleado.Name = "btnActualizarEmpleado";
+            this.btnActualizarEmpleado.Size = new System.Drawing.Size(163, 51);
+            this.btnActualizarEmpleado.TabIndex = 3;
+            this.btnActualizarEmpleado.Text = "Actualizar empleado";
+            this.btnActualizarEmpleado.UseVisualStyleBackColor = true;
+            this.btnActualizarEmpleado.Click += new System.EventHandler(this.btnActualizarEmpleado_Click);
+            // 
+            // btnEliminarEmpleado
+            // 
+            this.btnEliminarEmpleado.Location = new System.Drawing.Point(23, 231);
+            this.btnEliminarEmpleado.Name = "btnEliminarEmpleado";
+            this.btnEliminarEmpleado.Size = new System.Drawing.Size(163, 51);
+            this.btnEliminarEmpleado.TabIndex = 2;
+            this.btnEliminarEmpleado.Text = "\tEliminar empleado";
+            this.btnEliminarEmpleado.UseVisualStyleBackColor = true;
+            this.btnEliminarEmpleado.Click += new System.EventHandler(this.btnEliminarEmpleado_Click);
+            // 
+            // btnExportarCSV
+            // 
+            this.btnExportarCSV.Location = new System.Drawing.Point(23, 305);
+            this.btnExportarCSV.Name = "btnExportarCSV";
+            this.btnExportarCSV.Size = new System.Drawing.Size(163, 51);
+            this.btnExportarCSV.TabIndex = 5;
+            this.btnExportarCSV.Text = "\tExportar a CSV";
+            this.btnExportarCSV.UseVisualStyleBackColor = true;
+            this.btnExportarCSV.Click += new System.EventHandler(this.btnExportarCSV_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(23, 377);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(163, 51);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelMenu);
+            this.IsMdiContainer = true;
+            this.Name = "FormMain";
+            this.Text = "Gestor de Empleados.\n";
+            this.panelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnExportarCSV;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnActualizarEmpleado;
+        private System.Windows.Forms.Button btnEliminarEmpleado;
+        private System.Windows.Forms.Button btnAgregarEmpleado;
+        private System.Windows.Forms.Button btnMostrarEmpleados;
+    }
+}
+
---
+++ GestorEmpleados/GestorEmpleados/FormMain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestorEmpleados
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            IsMdiContainer = true;
+        }
+        
+
+     
+
+        private void btnMostrarEmpleados_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario(new FormMostrarEmpleados());
+        }
+
+        private void btnAgregarEmpleado_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario(new FormAgregarEmpleado());
+        }
+
+        private void btnActualizarEmpleado_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario(new FormActualizarEmpleado());
+        }
+
+        private void btnEliminarEmpleado_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario(new FormEliminarEmpleado());
+        }
+
+        private void btnExportarCSV_Click(object sender, EventArgs e)
+        {
+            AbrirFormulario(new FormExportarCSV());
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void AbrirFormulario(Form formularioHijo)
+        {
+            // Cierra formularios hijos abiertos
+            foreach (Form frm in this.MdiChildren)
+            {
+                frm.Close();
+            }
+
+            formularioHijo.MdiParent = this;
+            formularioHijo.Dock = DockStyle.Fill;
+            formularioHijo.Show();
+        }
+
+    }
+}
